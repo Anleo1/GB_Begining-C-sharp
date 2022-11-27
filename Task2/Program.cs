ec4580891c52fdf06690ed0cce6db07ce8a61957
Anastasia <anastpcher@rambler.ru>
2022-11-28 01:40:56 +0300

    Add first homework

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int max = firstNumber;
+ if(max < secondNumber) max = secondNumber;
+
+Console.WriteLine($"max = {max}");
---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int maximum = firstNum;
+if (maximum < secondNum) {
+    maximum = secondNum;
+    if (maximum < thirdNum) maximum =thirdNum;
+}
+else if (maximum < thirdNum) maximum =thirdNum;
+
+Console.WriteLine($"maximum = {maximum}");
