18938faefe2fe81779f36a267e939f6cae418831
Anastasia <95341361+Anleo1@users.noreply.github.com>
2022-11-28 14:09:38 +0300

    Update Program.cs

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -7,6 +7,6 @@ int thirdNum = Convert.ToInt32(Console.ReadLine());
 
 int maximum = firstNum;
 if (maximum < secondNum) maximum = secondNum;
-if (maximum < thirdNum) maximum =thirdNum;
+if (maximum < thirdNum) maximum = thirdNum;
 
 Console.WriteLine($"maximum = {maximum}");
